3bb8225524e734438d8cc4f5a648f910bafe86ba
Drew <drewcronshaw@edu.selkirk.ca>
2019-12-11 14:40:01 -0800

    fixed everything

--- header.php
+++ header.php
@@ -6,9 +6,9 @@
 <!-- Nav -->
     <nav id="nav">
         <ul>
-            <li id="homeLink"><a href="index.php">Home</a></li>
+            <li id="homeLink"><a href="index.php">Bome</a></li>
             <li id="learn-moreLink"><a href="learn-more.php">Learn More</a></li>
-            <li id="demoLink"><a href="demo.php">Demo</a></li>
+            <li id="demoLink"><a href="demo.php">Damo</a></li>
             <li id="contactLink"><a href="contact.php">Contact Us</a></li>
         </ul>
         
